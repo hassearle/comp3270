7864355d7b2b3cae42a4b0ba93e2b1a68fceb322
Ash <a.searle274@gmail.com>
2018-04-02 15:13:41 -0500

    trying to convert static to non-static method

--- Algo.java
+++ Algo.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 
 public class Algo {
 
@@ -18,7 +19,6 @@ public class Algo {
       algorithms.
       */
       
-      //fix
       int[] arr = new int[10];
       int count = 0;
       
@@ -35,27 +35,40 @@ public class Algo {
       }
       br.close();
       
-      
-      int algo1 = algo1(arr);
+      algo1(arr);
+      //int algo1 = algo1(arr);
       int algo2 = algo2(arr);
-      int algo3 = maxSum(arr, 0, 0);
+      int algo3 = maxSum(arr, 2, 4);
       int algo4 = algo4(arr);
-      // for(int i=0; i < arr.length ; i++){
-   //       System.out.println(arr[i]);
-   //    };
    
       System.out.println("algorithm-1: " + algo1);
       System.out.println("algorithm-2: " + algo2);
       System.out.println("algorithm-3: " + algo3);
       System.out.println("algorithm-4: " + algo4 
-         + " where " + algo4 + "is the MSCS as " +
+         + " where " + algo4 + " is the MSCS as " +
          "determined by each of the algorithms.");
       
    }
    
-   public static int algo1(int[] X){
-      int P = X[0];
-      int Q = X[X.length];
+   public static int max(int x, int y){
+      if(x > y){ 
+         return x;
+      }
+      return y;
+   }
+   public static int max(int x, int y, int z){
+      if(x > y || x > z){
+         return x;
+      }
+      else if (y > z || y > x){
+         return y;
+      }
+      return z; 
+   }
+   
+   public int algo1(int[] X){
+      int P = 0;
+      int Q = X.length-1;
       int maxSoFar = 0;
       for(int L=P; L<Q; L++){
          for(int U=L; U<Q; U++){
@@ -64,7 +77,7 @@ public class Algo {
                sum = sum + X[I];
             //sum now contains the sum of X[L..U]
             }
-         //maxSoFar = max(maxSoFar, sum)
+            maxSoFar = max(maxSoFar, sum);
          }
       }
       return maxSoFar;
@@ -72,17 +85,17 @@ public class Algo {
 
    public static int algo2(int[] X){
       int maxSoFar = 0; 
-      int P = X[0];
-      int Q = X[X.length];
+      int P = 0;
+      int Q = X.length-1;
       for(int L=P; L<Q; L++){
          int sum = 0;
          for(int U=L; U<Q; U++){
             sum = sum + X[U];
             // sum now contains the sum of X[L..U]
-            //maxSoFar = max(maxSoFar, sum);
+            maxSoFar = max(maxSoFar, sum);
          }
       }
-      return 0;
+      return maxSoFar;
    }
    
    //algo3
@@ -93,7 +106,7 @@ public class Algo {
          return 0; //zero-element vector
       }
       if(L == U){
-         return 0;//max(0,X[L]); //one-element vector
+         return max(0,X[L]); //one-element vector
       }
       int M = (L+U)/2; //A is X[L..M], B is X[M+1..U]
       //Find max crossing to left
@@ -101,31 +114,31 @@ public class Algo {
       int maxToLeft = 0;
       for(int I=M; I>L; I--){
          sum = sum + X[I];
-         maxToLeft = 0;//max(maxToLeft, sum);
+         maxToLeft = max(maxToLeft, sum);
       }
       //Find max crossing to right
       sum = 0; 
       int maxToRight = 0;
       for(int I = M+1; I<U; I++){
          sum = sum + X[I];
-         maxToRight = 0;//max(maxToRight, sum);
+         maxToRight = max(maxToRight, sum);
       }
       int maxCrossing = maxToLeft + maxToRight;
       int maxInA = maxSum(X,L,M);
       int maxInB = maxSum(X,M+1,U);
       
-      return 0;//max(maxCrossing, maxInA, maxInB);
+      return max(maxCrossing, maxInA, maxInB);
    }
    
    public static int algo4(int[] X){
       int maxSoFar = 0;
       int maxEndingHere = 0;
-      int P = X[0];
-      int Q = X[X.length];
+      int P = 0;
+      int Q = X.length-1;
       for(int I=P; I<Q; I++){
-         //maxEndingHere = max(0, maxEndingHere + X[I])
-         //maxSoFar = max(maxSoFar, maxEndingHere)
+         maxEndingHere = max(0, maxEndingHere + X[I]);
+         maxSoFar = max(maxSoFar, maxEndingHere);
       }
-      return 0;
+      return maxSoFar;
    }
 }
